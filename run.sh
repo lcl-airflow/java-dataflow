97275d09d199cb9f880d3d8dde9eb07821cdc6c5
herman.cheung@loblaw.ca <herman.cheung@loblaw.com>
2019-12-12 14:50:23 -0500

    ++run

---
+++ run.sh
@@ -0,0 +1,8 @@
+PROJECT=$(gcloud config list project --format "value(core.project)")
+gsutil mb gs://$PROJECT
+mvn compile exec:java -Dexec.mainClass=org.lcl.beam.template.WordCount \
+     -Dexec.args="--runner=DataflowRunner --project=$PROJECT \
+                  --gcpTempLocation=gs://$PROJECT/tmp \
+                  --inputFile=gs://apache-beam-samples/shakespeare/* --output=gs://$PROJECT/counts" \
+     -Pdataflow-runner
+
